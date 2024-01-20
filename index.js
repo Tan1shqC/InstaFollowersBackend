22f639222f9a473a41bb12825cb3a0d0563bc8d1
Tan1shqC <tanishqchoudhary1@gmail.com>
2024-01-20 11:31:07 +0530

    added script for chrome installation

--- index.js
+++ index.js
@@ -10,16 +10,13 @@ const fetchInstaPage = async (username) => {
     // const res = await fetch("https://readwell.onrender.com/");
     // const res = await fetch("https://www.instagram.com/tan1shq_c/");
     // const res = await fetch("https://www.instagram.com/chriswillx/");
-    await browser_1.install({
-        browser: browser_1.Browser.CHROME
-    });
-    
+
     const browser = await puppeteer.launch({
         headless: 'new'
     });
     const page = await browser.newPage();
     let followersCount = -1;
-    
+
     // Set the content of the page
     // await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4');
     await page.goto(`https://www.instagram.com/${username}/`, {
@@ -27,21 +24,21 @@ const fetchInstaPage = async (username) => {
     });
     const content = await page.content();
     // fs.writeFile('page.html', content, (err) => console.log(err));
-    
+
     const lis = await page.$$eval('meta', lis => {
         return lis.map(lis => lis.content.toLowerCase());
     });
     console.log(lis);
-    
+
     // Search the array for "followers"
     const followersSpan = lis.find(text => text.includes('followers'));
-    
+
     if (followersSpan) {
         // const text = "732 Followers, 753 Following, 3 Posts - See Instagram photos and videos from Tanishq Choudhary (@tan1shq_c)";
-        
+
         // Use a regular expression to extract the number before "Followers"
         const match = followersSpan.match(/([\w.]+)\sfollowers/);
-        
+
         if (match && match[1]) {
             followersCount = match[1];
             // followersCount = parseInt(match[1], 10);
@@ -49,11 +46,11 @@ const fetchInstaPage = async (username) => {
         } else {
             console.log('No followers count found.');
         }
-        
+
     } else {
         console.log('No element containing "followers" found.');
     }
-    
+
     // Close the browser
     await browser.close();
     return followersCount;
@@ -68,13 +65,18 @@ app.use(cors());
 
 // Define a route that handles POST requests with JSON payload
 app.get('/', async (req, res) => {
-  const username = req.query.username;
-  const followersCount = await fetchInstaPage(username);
-  console.log(followersCount);
-  res.json({ followersCount });
+    const username = req.query.username;
+    const followersCount = await fetchInstaPage(username);
+    console.log(followersCount);
+    res.json({ followersCount });
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(port, async () => {
+    await browser_1.install({
+        browser: browser_1.Browser.CHROME,
+        cacheDir: "/opt/render/.cache/puppeteer",
+        buildId: browser_1.resolveBuildId(browser_1.Browser.CHROME, browser_1.BrowserPlatform.LINUX, ""),
+    });
+    console.log(`Server listening at http://localhost:${port}`);
 });
